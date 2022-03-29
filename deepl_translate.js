336b4072f54fe2dfd7a399c25fb60873012c42c2
Kepler 69c <80059906+Kepler-69c@users.noreply.github.com>
2022-03-29 19:12:06 +0200

    Update deepl_translate.js
    the link to DeepL now fades out after a while

--- deepl_translate.js
+++ deepl_translate.js
@@ -4,18 +4,42 @@
 // @run-at document-start
 // ==/UserScript==
 
+function remUS(){
+  var UScript = document.getElementById('kepler-userscript'); //find previous link...
+  if (UScript !== null) { //... if it exists
+    UScript.remove(); // delete it
+  }
+}
+
+function fadeOut(){ //fade out
+  var Int = null;
+  var elem = document.getElementById("kepler-userscript");
+  var op = 1; //opacity is 1
+  Int = setInterval(fade, 100); //repeat until...
+  function fade() {
+    if (op <= 0.05) { //...opacity < 0.05
+      elem.remove(); //remove link
+      clearInterval(id); //clear interval
+    } else {
+      op = op - 0.05;
+      elem.style.opacity = op;
+    }
+  }
+}
+
     document.querySelector('body').addEventListener("mouseup", function() { //every time something gets clicked
+   
+    
       s = window.getSelection();
-      sText = s.toString(); //get selected Text
+      sText = s.toString(); //get selected Text that will get displayed
+      cText = s.toString(); //get selected Text
       oRange = s.getRangeAt(0); //get Text range
       oRect = oRange.getBoundingClientRect(); //get Text position
 
+
       if (sText !== '') { // if there is a selected text
 
-        var UScript = document.getElementById('kepler-userscript'); //find previous link...
-        if (UScript !== null) { //... if it exists
-          UScript.remove(); // delete it
-        }
+        remUS();//remove previous links, if they exist
 
         if (sText.length > 15) { //if text is too long, slice it
           sText = sText.slice(0, 14) + '...';
@@ -25,15 +49,16 @@
         var copyBox = document.createElement('a'); //create link to translator
 
         copyBox.innerText = sText; // set text
-        copyBox.setAttribute('href', 'https://www.deepl.com/en/translator#en/en/'+s.toString()+'/'); //set link
+        copyBox.setAttribute('href', 'https://www.deepl.com/en/translator#en/en/'+cText+'/'); //set link
         copyBox.setAttribute('target', '_blank'); //link should open in a new tab
         copyBox.setAttribute('id', 'kepler-userscript'); // mark as created by userscript
         copyBox.setAttribute('style', 'text-decoration:none;color:black;padding:5px 10px;border-radius: 3px;box-shadow: 1px 1px 4px #000;position:fixed;left:'+oRect.x+'px;top:'+oRect.y+'px;background:white;z-index:999;'); //set style
 
         document.getElementsByTagName('body')[0].append(copyBox); //append the link to the website
 
-        navigator.clipboard.writeText(s.toString()); //copy selection co clipboard
+        navigator.clipboard.writeText(cText); //copy selection co clipboard
         window.getSelection().removeAllRanges(); //remove selection
+        fadeOut(); //remove link
 
         //console.log('Left: '+oRect.x+'\nTop: '+oRect.y);
       }
