e41536bf27d72572cfe3ea03cd011662d397ee76
Kepler 69c <80059906+Kepler-69c@users.noreply.github.com>
2022-04-20 20:51:25 +0200

    Update focus.js

--- focus.js
+++ focus.js
@@ -5,84 +5,122 @@
 // @run-at document-start
 // ==/UserScript==
 
-  var gradientArray = [
-    'linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)',
-    'linear-gradient(to right, #780206, #061161)',
-    'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
-    'linear-gradient(180deg, #52ACFF 25%, #FFE32C 100%)',
-    'linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)',
-    'linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)',
-    'linear-gradient(132deg, #F4D03F 0%, #16A085 100%)',
-    'linear-gradient(132deg, #F4D03F 0%, #16A085 100%)',
-    'linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%)'
-  ];
+// What should be in the background?
+var keyword = 'wallpaper';
+// add button to continue to blocked website
+var showButton = true;
 
-  var quoteArray = [
-    "Opportunity is missed by most people because it is dressed in overalls and looks like work. – Thomas Edison",
-    "It seems the harder I work, the more luck I have. – Thomas Jefferson",
-    "Hard work spotlights the character of people: some turn up their sleeves, some turn up their noses, and some don’t turn up at all. – Sam Ewing",
-    "Nobody’s a natural. You work hard to get good and then work to get better. It’s hard to stay on top. – Paul Coffey",
-    "The only place success comes before work is in the dictionary. – Vincent “Vince” Lombardi",
-    "It is the working man who is the happy man. It is the idle man who is the miserable man. – Benjamin Franklin",
-    "If you are going to achieve excellence in big things, you develop the habit in little matters. Excellence is not an exception, it is a prevailing attitude. – Charles R. Swindoll",
-    "The secret of getting ahead is getting started. – Mark Twain",
-    "You can’t use up creativity. The more you use, the more you have. – Maya Angelou",
-    "Be miserable. Or motivate yourself. Whatever has to be done, it’s always your choice. – Wayne Dyer",
-    "Even if you fall on your face, you’re still moving forward. – Victor Kiam",
-    "Problems are not stop signs, they are guidelines. – Robert H. Schuller",
-    "Opportunity does not knock, it presents itself when you beat down the door. – Kyle Chandler",
-    "Act as if what you do makes a difference. It does. – William James",
-    "Quality is not an act, it is a habit. – Aristotle",
-    "Don’t wait. The time will never be just right. – Napoleon Hill",
-    "True happiness involves the full use of one’s power and talents. – John W. Gardner",
-    "Small deeds done are better than great deeds planned. – Peter Marshall",
-    "Build your own dreams, or someone else will hire you to build theirs.– Farrah Gray",
-    "Everything you’ve ever wanted is on the other side of fear. – George Addair",
+var gradientArray = [
+  'linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)',
+  'linear-gradient(to right, #780206, #061161)',
+  'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
+  'linear-gradient(180deg, #52ACFF 25%, #FFE32C 100%)',
+  'linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)',
+  'linear-gradient(0deg, #FFDEE9 0%, #B5FFFC 100%)',
+  'linear-gradient(132deg, #F4D03F 0%, #16A085 100%)',
+  'linear-gradient(132deg, #F4D03F 0%, #16A085 100%)',
+  'linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%)'
+];
 
-    "You can always find a distraction if you're looking for one. – Tom Kite",
-    "The Internet is a big distraction. – Ray Bradbury",
-    "Work is hard. Distractions are plentiful. And time is short. – Adam Hochschild",
-    "Believe you can and you’re halfway there. – Theodore Roosevelt",
-    "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it. – Henry Ford",
-    "Nothing will work unless you do. – Maya Angelou",
-    "The difference between the greats and the legends is their ability to focus for longer periods of time. – Jordan Burroughs"
-  ];
+var quoteArray = [
+  "Opportunity is missed by most people because it is dressed in overalls and looks like work. – Thomas Edison",
+  "It seems the harder I work, the more luck I have. – Thomas Jefferson",
+  "Hard work spotlights the character of people: some turn up their sleeves, some turn up their noses, and some don’t turn up at all. – Sam Ewing",
+  "Nobody’s a natural. You work hard to get good and then work to get better. It’s hard to stay on top. – Paul Coffey",
+  "The only place success comes before work is in the dictionary. – Vincent “Vince” Lombardi",
+  "It is the working man who is the happy man. It is the idle man who is the miserable man. – Benjamin Franklin",
+  "If you are going to achieve excellence in big things, you develop the habit in little matters. Excellence is not an exception, it is a prevailing attitude. – Charles R. Swindoll",
+  "The secret of getting ahead is getting started. – Mark Twain",
+  "You can’t use up creativity. The more you use, the more you have. – Maya Angelou",
+  "Be miserable. Or motivate yourself. Whatever has to be done, it’s always your choice. – Wayne Dyer",
+  "Even if you fall on your face, you’re still moving forward. – Victor Kiam",
+  "Problems are not stop signs, they are guidelines. – Robert H. Schuller",
+  "Opportunity does not knock, it presents itself when you beat down the door. – Kyle Chandler",
+  "Act as if what you do makes a difference. It does. – William James",
+  "Quality is not an act, it is a habit. – Aristotle",
+  "Don’t wait. The time will never be just right. – Napoleon Hill",
+  "True happiness involves the full use of one’s power and talents. – John W. Gardner",
+  "Small deeds done are better than great deeds planned. – Peter Marshall",
+  "Build your own dreams, or someone else will hire you to build theirs.– Farrah Gray",
+  "Everything you’ve ever wanted is on the other side of fear. – George Addair",
+  "You can always find a distraction if you're looking for one. – Tom Kite",
+  "The Internet is a big distraction. – Ray Bradbury",
+  "Work is hard. Distractions are plentiful. And time is short. – Adam Hochschild",
+  "Believe you can and you’re halfway there. – Theodore Roosevelt",
+  "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it. – Henry Ford",
+  "Nothing will work unless you do. – Maya Angelou",
+  "The difference between the greats and the legends is their ability to focus for longer periods of time. – Jordan Burroughs"
+];
 
-  //append motivating elements
-  function appendElements() {
-    var div = `
-      <div class="background" style="position:fixed;height:100vh;width:100vw;z-index:351;left:0;top:0;"></div>
-      <div class="inspire-content" style="display: block;text-align: center;padding-top: 50px;text-align: center;z-index:352;font-weight: 900;position: relative;min-height: 1px;padding-right: 15px;padding-left: 15px;">
-        <div class="quote-message" style="color: white;font-family: Montserrat, 'Helvetica Neue', Helvetica, Arial, sans-serif;font-size: 2.5vw;margin-left: 10%;margin-right: 10%; margin-top: 10%;"></div>
-      </div>`;
-      document.body.innerHTML = div;
-  };
+//create elements
+function create(type, attr2, style) {
+  let div = document.createElement(type);
+  div.setAttribute('class', attr2);
+  div.setAttribute('style', style);
+  return div;
+}
 
-  //choose a random quote
-  function randomQuote() {
-    var quote = quoteArray[Math.floor(Math.random() * quoteArray.length)];
-    var quote_div = "<div style='margin: 0 -0.4em;padding: 0.1em 0.4em; border-radius: 0.8em 0.3em; background: transparent; background-image: linear-gradient( to right, rgba(44, 130, 201, 0.1), rgba(44, 130, 201, 0.7) 4%, rgba(44,130, 201, 0.3) );-webkit-box-decoration-break: clone;'>"+quote+"</div>"
-    document.getElementsByClassName('quote-message')[0].innerHTML = quote_div;
-  };
+//append motivating elements
+function appendElements() {
 
-  //add a gradient to the website until the image loads
-  function randomGradient() {
-    var gradient = gradientArray[Math.floor(Math.random() * gradientArray.length)];
-    document.getElementsByClassName('background')[0].style.background = gradient;
-  };
+  // <div class="background" style="position:fixed;height:100vh;width:100vw;z-index:351;left:0;top:0;overflow:none;"></div>
+  // <div class="inspire-content" style="display: block;text-align: center;padding-top: 50px;text-align: center;z-index:352;font-weight: 900;position: relative;min-height: 1px;padding-right: 15px;padding-left: 15px;">
+  //   <div class="quote-message" style="color: white;font-family: Montserrat, 'Helvetica Neue', Helvetica, Arial, sans-serif;font-size: 2.5vw;margin-left: 10%;margin-right: 10%; margin-top: 10%;"></div>
+  // </div>`;
 
-  //when the image loads, append it to the website
-  function randomImage() {
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    var img2 = new Image();
-    img2.onload = function() {
-      document.getElementsByClassName('background')[0].appendChild(this);
-    }
-    img2.src = 'https://source.unsplash.com/random/'+w+'x'+h+'/?wallpapers';
-  };
+  var div = create('div', 'userscript-background', 'position:fixed;height:100vh;width:100vw;z-index:16777271;left:0;top:0;overflow:none;'); //z-index set to max (24-bit value)
+  var inspire = create('div', 'userscript-inspire-content', 'display: block;text-align: center;padding-top: 50px;text-align: center;z-index:352;font-weight: 900;position: relative;min-height: 1px;padding-right: 15px;padding-left: 15px;')
+  var quote = create('div', 'userscript-quote-message', 'color: white;font-family: Montserrat, "Helvetica Neue", Helvetica, Arial, sans-serif;font-size: 2.5vw;margin-left: 10%;margin-right: 10%; margin-top: 10%;');
+
+  inspire.append(quote);
+  div.append(inspire);
+  document.body.prepend(div);
+};
+
+//choose a random quote
+function randomQuote() {
+  var quote = quoteArray[Math.floor(Math.random() * quoteArray.length)];
+  var quote_div = "<div style='margin: 0 -0.4em;padding: 0.1em 0.4em; border-radius: 0.8em 0.3em; background: transparent; background-image: linear-gradient( to right, rgba(44, 130, 201, 0.1), rgba(44, 130, 201, 0.7) 4%, rgba(44,130, 201, 0.3) );-webkit-box-decoration-break: clone;'>"+quote+"</div>"
+  document.getElementsByClassName('userscript-quote-message')[0].innerHTML = quote_div;
+};
+
+//add a gradient to the website until the image loads
+function randomGradient() {
+  var gradient = gradientArray[Math.floor(Math.random() * gradientArray.length)];
+  document.getElementsByClassName('userscript-background')[0].style.background = gradient;
+};
+
+//when the image loads, append it to the website
+function randomImage() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  var img2 = new Image();
+  img2.onload = function() {
+    this.setAttribute('style', 'position:fixed;top:0;left:0;width:100%;height:100%;');
+    document.getElementsByClassName('userscript-background')[0].appendChild(this);
+  }
+  img2.src = 'https://source.unsplash.com/random/'+w+'x'+h+'/?'+keyword;
+};
+
+//append button to continue to website
+function appendButton() {
+  var button = create('div', 'userscript-button', 'font-weight:900;position:absolute;bottom:5vh;left:50%;transform:translateX(-50%);z-index:2;color: white;font-family: Montserrat, "Helvetica Neue", Helvetica, Arial, sans-serif;font-size: 2.5vw;background: transparent;background-image: linear-gradient( to right, rgba(44, 130, 201, 0.1), rgba(44, 130, 201, 0.7) 4%, rgba(44,130, 201, 0.3) );-webkit-box-decoration-break: clone;margin: 0 -0.4em;padding: 0.1em 0.4em;border-radius: 0.8em 0.3em;user-select:none;cursor:pointer;');
+  button.innerHTML = 'Continue to Website';
+  document.getElementsByClassName('userscript-background')[0].append(button);
+  document.getElementsByClassName('userscript-button')[0].addEventListener('click', removeQuote, false);
+}
+
+function removeQuote() {
+  document.getElementsByClassName('userscript-background')[0].remove();
+}
+
+console.log('debug');
 
   appendElements();
   randomGradient();
   randomQuote();
   randomImage();
+
+if (showButton) {
+  appendButton();
+}
